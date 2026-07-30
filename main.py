f8a48fd30ae714f6c7c40a715068a56aaf1198a9
kranz-fafka <mikolajbogusz-2000@wp.pl>
2026-07-30 22:17:24 +0200

    Dodanie wprowadzania odpowiedzi przez użytkownika

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 print("To jest projekt do monitorowania wydatków \n")
 
-print("1. Dodaj wydatek")
-print("2. Pokaż wydatki")
-print("3. Wyjście")
+def show_menu():
+    print("1. Dodaj wydatek")
+    print("2. Pokaż wydatki")
+    print("3. Wyjście")
+    print("\n")
 
-input("Wybierz co chcesz zrobić: ")
+def main():
+    
+    while True:
+        show_menu()
+
+        choice = input("Wybierz co chcesz zrobić: ").strip()
+
+        if choice == "1":
+            print("Dodawanie wydatku")
+        elif choice == "2":
+            print("Wyświetlanie wydatków")
+        elif choice == "3":
+            print("Zamknięcie programu")
+            break
+        else:
+            print("Błędny wybór! Spróbuj ponownie")
+
+if __name__ == "__main__":
+    main()
