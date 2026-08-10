656d7889d23dc088dc4d2ccd82957875be9b43e8
kranz-fafka <mikolajbogusz-2000@wp.pl>
2026-08-10 15:48:51 +0200

    Dodawanie wydatku

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import datetime
+
 print("To jest projekt do monitorowania wydatków \n")
 
 def show_menu():
@@ -5,6 +7,36 @@ def show_menu():
     print("2. Pokaż wydatki")
     print("3. Wyjście")
     print(" ")
+
+def add_expense(expenses_list):
+    print("Dodawanie nowego wydatku")
+
+    while True:
+        try:
+            amount_input = input("Podaj kwotę: ").replace(",", ".").strip()
+            amount = float(amount_input)
+            break
+        except ValueError:
+            print("Błąd! Podaj wartość w formiacie 00.00!")
+
+    category = input("Wpisz kategorię: ").strip()
+    date = input("Podaj datę w formacie DD-MM-YYYY: ").strip()
+    description = input("Dodaj opis: ").strip()
+
+    if description == "":
+        description = "BRAK"
+    if date == "":
+        date = datetime.date.today().strftime("%d-%m-%Y")
+
+    new_expense = {
+        "Kwota" : amount,
+        "Kategoria" : category,
+        "Data" : date,
+        "Opis" : description
+    }
+
+    expenses_list.append(new_expense)
+    print("Wydatek został pomyślnie dodany!\n")
 
 def main():
 
@@ -17,10 +49,15 @@ def main():
         choice = input("Wybierz co chcesz zrobić: ").strip()
 
         if choice == "1":
-            print("Dodawanie wydatku\n")
+            add_expense(expenses)
         elif choice == "2":
             #dodać obsługę pustej listy
             print("Wyświetlanie wydatków\n")
+            if len(expenses) == 0:
+                print("Brak wydatków\n")
+            else:
+                print(f"Liczba dotychczasowych wydatkó: {len(expenses)}")
+                print(expenses)
         elif choice == "3":
             print("Zamknięcie programu\n")
             break
